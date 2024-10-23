5a557d6b5347e1ec9b8a9224725abcbd53305040
Azmyin <azmyin12@gmail.com>
2024-10-23 21:06:10 +0600

    added robot_yaml in BaseGenerator class

--- clearpath_generator_common/clearpath_generator_common/common.py
+++ clearpath_generator_common/clearpath_generator_common/common.py
@@ -1,30 +1,39 @@
-# Software License Agreement (BSD)
-#
-# @author    Roni Kreinin <rkreinin@clearpathrobotics.com>
-# @copyright (c) 2023, Clearpath Robotics, Inc., All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-# * Redistributions of source code must retain the above copyright notice,
-#   this list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
-# * Neither the name of Clearpath Robotics nor the names of its contributors
-#   may be used to endorse or promote products derived from this software
-#   without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
+#!/usr/bin/env python3
+
+"""
+Software License Agreement (BSD)
+
+Copyright 2023, Clearpath Robotics, Inc., All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+- Redistributions of source code must retain the above copyright notice,
+  this list of conditions, and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions, and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+- Neither the name of Clearpath Robotics nor the names of its contributors
+  may be used to endorse or promote products derived from this software
+  without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+Original Author: Roni Kreinin <rkreinin@clearpathrobotics.com>
+Gazebo Harmonic integration by: Azmyin Md. Kamal <azmyin12@gmail.com>
+"""
+
+
+# Imports
 import getopt
 import os
 import sys
@@ -276,10 +285,18 @@ class BaseGenerator():
     LAUNCH_PATH = 'launch/'
     PARAM_PATH = 'config/'
 
-    def __init__(self,
-                 setup_path: str = '/etc/clearpath/') -> None:
+    # def __init__(self,
+    #              setup_path: str = '/etc/clearpath/') -> None:
+    def __init__(self, setup_path: str = '/etc/clearpath/', robot_yaml = 'robot.yaml') -> None:
         # Define paths
-        self.config_path = os.path.join(setup_path, 'robot.yaml')
+        self.robot_yaml_file = robot_yaml # Initialize
+        # self.config_path = os.path.join(setup_path, 'robot.yaml')
+        self.config_path = os.path.join(setup_path, self.robot_yaml_file)
+        
+        print()
+        print(f"BaseGenerator: self.config_path --> {self.config_path}")
+        print()
+
         assert os.path.exists(self.config_path)
 
         self.setup_path = setup_path
@@ -319,7 +336,9 @@ class BaseGenerator():
 
     @staticmethod
     def get_args():
+        """Get argument values from robot_spawn.launch file."""
         # Get options
+        # If --ros-args is not found, it considers all other argument passing format
         try:
             last_arg_index = sys.argv.index('--ros-args')
         except ValueError:
@@ -327,16 +346,22 @@ class BaseGenerator():
         argv = sys.argv[1:last_arg_index]
 
         try:
-            options, args = getopt.getopt(argv, 's:', ['setup_path='])
+            #options, args = getopt.getopt(argv, 's:', ['setup_path='])
+            options, args = getopt.getopt(argv, 's:r:', ['setup_path=', 'robot_yaml='])
         except getopt.GetoptError as err:
             print(err)
 
+        # Default values
         setup_path = '/etc/clearpath/'
+        robot_yaml = 'robot.yaml'
 
         for option, value in options:
             if option in ('-s', '--setup_path'):
                 setup_path = value
+            if option in ('-r', '--robot_yaml'):
+                robot_yaml = value
             else:
+                # TODO raise a ValueNotFound error
                 pass
 
-        return setup_path
+        return setup_path, robot_yaml
